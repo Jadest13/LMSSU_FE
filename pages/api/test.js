debf185000a33cd39f577af6061bcb92612f6280
Jadest <jadest13x03@gmail.com>
2022-12-07 17:07:53 +0900

    test5

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,6 +2,5 @@
 export default async function handler(req, res) {
   const { studentId, pwd } = req.body;
   console.log(req.body);
-  //res.status(200).json({ id: studentId, pwd: pwd, status: "OK" })
-  return res.send('asdasdhello');
+  res.status(200).json({ id: studentId, pwd: pwd, status: "OK" })
 }
