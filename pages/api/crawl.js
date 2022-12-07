ff6e47d256d831e45e2e228236f400832c5da02c
Jadest <jadest13x03@gmail.com>
2022-12-07 17:11:52 +0900

    test5

--- pages/api/crawl.js
+++ pages/api/crawl.js
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer'
+// import puppeteer from 'puppeteer'
 
 export default async function handler(req, res) {
 
@@ -7,35 +7,35 @@ export default async function handler(req, res) {
     res.status(200).json({ id: studentId, pwd: pwd, status: "OK" })
     console.log(req.body)
 
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
-    const ndhs_id = studentId; // 추후 로그인 폼에서 각자의 아이디 비밀번호를 입력받게 할 예정
-    const ndhs_pw = pwd;
+    // const browser = await puppeteer.launch({headless: true});
+    // const page = await browser.newPage();
+    // const ndhs_id = studentId; // 추후 로그인 폼에서 각자의 아이디 비밀번호를 입력받게 할 예정
+    // const ndhs_pw = pwd;
 
-    page.setDefaultNavigationTimeout(5000); 
+    // page.setDefaultNavigationTimeout(5000); 
 
-    await page.goto('https://smartid.ssu.ac.kr/Symtra_sso/smln.asp?apiReturnUrl=https%3A%2F%2Fclass.ssu.ac.kr%2Fxn-sso%2Fgw-cb.php');
+    // await page.goto('https://smartid.ssu.ac.kr/Symtra_sso/smln.asp?apiReturnUrl=https%3A%2F%2Fclass.ssu.ac.kr%2Fxn-sso%2Fgw-cb.php');
     
-    await page.evaluate((id, pw) => {
-      document.querySelector('#userid').value = id;
-      document.querySelector('#pwd').value = pw;
-    }, ndhs_id, ndhs_pw);
+    // await page.evaluate((id, pw) => {
+    //   document.querySelector('#userid').value = id;
+    //   document.querySelector('#pwd').value = pw;
+    // }, ndhs_id, ndhs_pw);
 
 
-    await page.click('#sLogin > div > div:nth-child(1) > form > div > div:nth-child(2) > a');
+    // await page.click('#sLogin > div > div:nth-child(1) > form > div > div:nth-child(2) > a');
 
-    await page.waitForNavigation().then(() => {
-      console.log('success')
-    }).catch((res) => {
-        console.log('fails', res)
-    })
+    // await page.waitForNavigation().then(() => {
+    //   console.log('success')
+    // }).catch((res) => {
+    //     console.log('fails', res)
+    // })
 
-    if(page.url() === 'https://class.ssu.ac.kr/'){
-      await browser.close();
-      res.status(200).json({ id: ndhs_id, pwd: ndhs_pw, status: "OK" })
-    } else {
-      await browser.close();
-      res.status(200).json({ id: ndhs_id, pwd: ndhs_pw, status: "FAIL" })
-    }
+    // if(page.url() === 'https://class.ssu.ac.kr/'){
+    //   await browser.close();
+    //   res.status(200).json({ id: ndhs_id, pwd: ndhs_pw, status: "OK" })
+    // } else {
+    //   await browser.close();
+    //   res.status(200).json({ id: ndhs_id, pwd: ndhs_pw, status: "FAIL" })
+    // }
   }
 }
